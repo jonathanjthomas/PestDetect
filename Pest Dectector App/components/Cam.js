c5da60c325f31d0b24fbd6a4b0ffdb5a33eb6cf3
Cybertechy <siddhtailor96@gmail.com>
2023-07-24 17:20:37 +0400

    Dependency Fix

---
+++ Pest Dectector App/components/Cam.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const Cam = () => {
+  return (
+    <View>
+      <Text>Cam</Text>
+    </View>
+  );
+};
+
+export default Cam;
